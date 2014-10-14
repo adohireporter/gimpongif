5c52b388c072c44233019ec9aa1bd29b53046f45
adohireporter <adohireporter@gmail.com>
2014-10-14 11:56:16 -0400

    gfycat

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,9 @@ function gestureStart() {
     }
   }
 }
+(function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
+    s.parentNode.insertBefore(g, s);
+}(document, 'script'));
